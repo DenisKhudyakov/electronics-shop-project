04c32b07ad2576f349780fa266a8123dbcfe66a5
denishudyakov <denis.hudyakov27@gmail.com>
2023-12-13 23:44:42 +0500

    task1 done

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,8 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.mark.parametrize("name, price, quantity, result", [("Смартфон", 10000, 20, 10000)])
+def test_class(name, price, quantity, result):
+    assert Item(name, price, quantity).apply_discount() == result
